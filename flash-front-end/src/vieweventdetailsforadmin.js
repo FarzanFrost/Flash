2bebb9b565c29f36a4cdf1382e0774368363c604
Thineshkanth <thineshkanth3759@gmail.com>
2022-07-15 22:40:38 +0530

    create a viewdetailforadmin.js file with reactjs structure

---
+++ flash-front-end/src/vieweventdetailsforadmin.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const vieweventdetailsforadmin = () => {
+
+    return (
+
+        <div>
+
+            vieweventdetailsforadmin
+
+        </div>
+
+    )
+
+}
+
+export default vieweventdetailsforadmin
