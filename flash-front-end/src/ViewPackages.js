fd7d2b85e99884d489925d5d28771f80317f412d
rahuram <rahuram66@gmail.com>
2022-07-18 13:04:09 +0530

    Rename the project

---
+++ flash-front-end/src/ViewPackages.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const ViewPackages = () => {
+
+    return (
+
+        <div>
+
+            <h1>ViewPackages</h1>
+
+        </div>
+
+    )
+
+}
+
+export default ViewPackages
