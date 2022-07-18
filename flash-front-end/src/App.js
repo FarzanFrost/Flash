7a265d759b51578f365e2f040c7dc5fc2f617646
nitharsha <nitharshathev@gmail.com>
2022-07-18 18:38:52 +0530

    Add new employee page and add a new photo

--- flash-front-end/src/App.js
+++ flash-front-end/src/App.js
@@ -5,6 +5,8 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import { BrowserRouter , Routes , Route } from "react-router-dom";
 
 import Login from "./Login";
+import AddEmployee from "./AddEmployee";
+
 function App() {
   return (
     <div /*className="App"*/>
@@ -22,12 +24,14 @@ function App() {
           Learn React
         </a>
       </header>*/}
+        <AddEmployee />
 
       <BrowserRouter>
 
           <Routes>
 
               <Route exact path="/Login" element={ <Login/> } />
+              <Route exact path="/AddEmployee" element={ <AddEmployee/> } />
 
           </Routes>
 
