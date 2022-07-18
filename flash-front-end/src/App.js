7577042dcac49b1f61897e64bdd09b7018169309
Thineshkanth <thineshkanth3759@gmail.com>
2022-07-18 16:32:34 +0530

    create a frame of vieweventdetailsmain page with bootstrap in here i create dropdown and cards with slideshows

--- flash-front-end/src/App.js
+++ flash-front-end/src/App.js
@@ -2,9 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
+
+
 import { BrowserRouter , Routes , Route } from "react-router-dom";
 
 import Login from "./Login";
+
+import Vieweventdetailsforadmin from "./Vieweventdetailsforadmin";
+
+import Sample from "./Sample";
+
 function App() {
   return (
     <div /*className="App"*/>
@@ -28,6 +35,8 @@ function App() {
           <Routes>
 
               <Route exact path="/Login" element={ <Login/> } />
+              <Route exact path="/Vieweventdetailsforadmin" element={ <Vieweventdetailsforadmin/> } />
+              <Route exact path="/Sample" element={ <Sample/> } />
 
           </Routes>
 
--- flash-front-end/src/Login.js
+++ flash-front-end/src/Login.js
@@ -8,6 +8,7 @@ const Login = () => {
 
             Login
 
+
         </div>
 
     )
