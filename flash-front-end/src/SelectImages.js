c7f9dfb3a1496624f788490081605d6099f2e0d3
Farzan <farzanroxz123@gmail.com>
2022-08-04 13:49:05 +0530

    added file to create customer select images

---
+++ flash-front-end/src/SelectImages.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const SelectImages = () => {
+
+
+    return (
+
+        <div>
+
+            ;asdlkjf;alskdjf;lkasdj
+
+        </div>
+
+    )
+
+}
+
+export default SelectImages
