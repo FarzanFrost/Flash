6979f17972e9ef84ccf94eb1d13795f91c5e44f0
Farzan <farzanroxz123@gmail.com>
2022-08-06 12:44:30 +0530

    completed to display dynamically the reviews and ratings

--- flash-front-end/src/ContextFiles/EventReviewsCustomerContext.js
+++ flash-front-end/src/ContextFiles/EventReviewsCustomerContext.js
@@ -4,16 +4,60 @@ export const EventReviewsCustomerContext = createContext( undefined )
 
 const EventReviewsCustomerContextProvider = ( props ) => {
 
+    const reviews = [
+
+        {
+
+            eventType : "Birthday", reviewContent : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod eos id officiis hic tenetur quae quaerat ad velit ab hic tenetur.",
+            rating : 5 , eventId : 1
+
+        },
+
+        {
+
+            eventType : "Wedding", reviewContent : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod eos id officiis hic tenetur quae quaerat ad velit ab hic tenetur.",
+            rating : 4 , eventId : 1
+
+        },
+
+        {
+
+            eventType : "Anniversary", reviewContent : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod eos id officiis hic tenetur quae quaerat ad velit ab hic tenetur.",
+            rating : 3 , eventId : 1
+
+        },
+
+        {
+
+            eventType : "Batch Meet", reviewContent : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod eos id officiis hic tenetur quae quaerat ad velit ab hic tenetur.",
+            rating : 2 , eventId : 1
+
+        },
+        {
+
+            eventType : "DJ party", reviewContent : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod eos id officiis hic tenetur quae quaerat ad velit ab hic tenetur.",
+            rating : 1 , eventId : 1
+
+        },
+        {
+
+            eventType : "DJ party", reviewContent : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod eos id officiis hic tenetur quae quaerat ad velit ab hic tenetur.",
+            rating : 0 , eventId : 1
+
+        }
+
+    ]
+
     return(
 
-        <EventReviewsCustomerContext.Provider value={ {} }>
+        <EventReviewsCustomerContext.Provider value={ { reviews } }>
 
             { props.children }
 
         </EventReviewsCustomerContext.Provider>
 
     )
-    
+
 }
 
 export default EventReviewsCustomerContextProvider
--- flash-front-end/src/EventReviewsCustomer.js
+++ flash-front-end/src/EventReviewsCustomer.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import {Link} from "react-router-dom";
+import {EventReviewsCustomerContext} from "./ContextFiles/EventReviewsCustomerContext";
 
 const EventReviewsCustomer = () => {
+
+    const { reviews } = useContext( EventReviewsCustomerContext )
+
+    const star = "bi bi-star text-warning"
+
+    const fillStar = "bi bi-star-fill text-warning"
 
     return(
 
@@ -18,39 +25,51 @@ const EventReviewsCustomer = () => {
                 <section>
 
                     <div className="row text-center">
-                        <div className="col-md-4 mb-5 mb-md-0 shadow-lg p-4">
 
-                            <h2 className="h2"> Birthday </h2>
-                            <p className="px-xl-3">
-                                <i className="fas fa-quote-left pe-2"></i>Lorem ipsum dolor sit amet, consectetur
-                                adipisicing elit. Quod eos id officiis hic tenetur quae quaerat ad velit ab hic
-                                tenetur.
-                            </p>
-                            <ul className="list-unstyled d-flex justify-content-center mb-0">
-                                <li>
-                                    <i className="bi bi-star-fill text-warning"></i>
-                                </li>
-                                <li>
-                                    <i className="bi bi-star-fill text-warning"></i>
-                                </li>
-                                <li>
-                                    <i className="bi bi-star-fill text-warning"></i>
-                                </li>
-                                <li>
-                                    <i className="bi bi-star-fill text-warning"></i>
-                                </li>
-                                <li>
-                                    <i className="bi bi-star text-warning"></i>
-                                </li>
-                            </ul>
+                        { reviews.length === 0 && <div> No Event Reviews Available </div> }
+                        { reviews.length > 0 &&
 
-                            <div>
+                            reviews.map( ( review ) => (
 
-                                <Link to={ "/" }> Event Details </Link>
+                                <div className="col-md-4 my-3 shadow-lg p-4">
 
-                            </div>
+                                    <h2 className="h2"> { review.eventType } </h2>
+                                    <p className="px-xl-3">
+                                        <i className="fas fa-quote-left pe-2"></i>Lorem ipsum dolor sit amet, consectetur
+                                        adipisicing elit. Quod eos id officiis hic tenetur quae quaerat ad velit ab hic
+                                        tenetur.
+                                    </p>
+                                    <ul className="list-unstyled d-flex justify-content-center mb-0">
 
-                        </div>
+                                        <li>
+                                            <i className={ ( 0 < review.rating ) ? fillStar:star }></i>
+                                        </li>
+                                        <li>
+                                            <i className={ ( 1 < review.rating ) ? fillStar:star }></i>
+                                        </li>
+                                        <li>
+                                            <i className={ ( 2 < review.rating ) ? fillStar:star }></i>
+                                        </li>
+                                        <li>
+                                            <i className={ ( 3 < review.rating ) ? fillStar:star }></i>
+                                        </li>
+                                        <li>
+                                            <i className={ ( 4 < review.rating ) ? fillStar:star }></i>
+                                        </li>
+                                    </ul>
+
+                                    <div>
+
+                                        <Link to={ "/" }> Event Details </Link>
+
+                                    </div>
+
+                                </div>
+
+                            ) )
+
+                        }
+
                     </div>
                 </section>
 
