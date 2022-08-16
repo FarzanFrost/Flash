d84e46e2d749bed251f491f6b9cb69b062772ce2
Thineshkanth <thineshkanth3759@gmail.com>
2022-08-16 21:47:18 +0530

    In here I add a table viewing all details about employees

--- flash-front-end/src/App.js
+++ flash-front-end/src/App.js
@@ -20,6 +20,7 @@ import EditEmployee from "./EditEmployee";
 import AdminViewEmployeeDetailspage from "./AdminViewEmployeeDetailspage";
 import SelectImageContextProviderInterface from "./SelectImageContextProviderInterface";
 import EventReviewsCustomerContextProviderInterface from "./EventReviewsCustomerContextProviderInterface";
+import ManagerViewEmployeeDetails from "./ManagerViewEmployeeDetails";
 
 
 function App() {
@@ -63,6 +64,7 @@ function App() {
               <Route exact path="/EditEmployee" element={ <EditEmployee/> } />
               <Route exact path="/Gallery" element={ <SelectImageContextProviderInterface/> } />
               <Route exact path="/EventReviews" element={ <EventReviewsCustomerContextProviderInterface/> } />
+              <Route exact path="/ManagerViewEmployeeDetails" element={ <ManagerViewEmployeeDetails/> } />
 
 
           </Routes>
