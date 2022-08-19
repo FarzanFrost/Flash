006c4e5bf0df0a7bd5301b59c87adcfb431d21f3
rahuram <rahuram66@gmail.com>
2022-08-19 13:25:54 +0530

    reedit view other packages

--- flash-front-end/src/ViewPackagesOther.js
+++ flash-front-end/src/ViewPackagesOther.js
@@ -11,7 +11,7 @@ const Vieweventdetailsforadmin = () => {
 
     const style1 = {
 
-        backgroundColor: '#B36A5E',
+        backgroundColor: '#767676',
         height:'850px'
     };
 
@@ -33,31 +33,7 @@ const Vieweventdetailsforadmin = () => {
         <div style={{...style1}}>
 
 
-            <div className=" row">
-                <div className="col-md-6">
-                    <div className="position-relative mx-5 m-2" style={{ width: '48rem',left:'16rem'}}>
-                        <input type="text" className="form-control" id="formGroupExampleInput" placeholder="Example input"></input>
-                    </div>
-                </div>
 
-
-
-                <div className="col-md-6" >
-
-                    <div className="dropdown position-relative mx-5 m-2" style={{ left:'16rem'}}>
-
-                        <select class="btn btn-secondary dropdown-toggle" type="button" id="Filter"
-                                data-bs-toggle="dropdown">
-                            <option value="All">All</option>
-                            <option value="eventname">Event Name</option>
-                            <option value="Date">Date</option>
-
-                        </select>
-                    </div>
-
-                </div>
-
-            </div>
 
             <section className="pt-5 pb-5">
                 <div className=" container">
