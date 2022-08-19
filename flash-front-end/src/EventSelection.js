c129638b1162a492a282dfb46bf99493da65a762
rahuram <rahuram66@gmail.com>
2022-08-19 16:00:40 +0530

    creating event selection

--- flash-front-end/src/EventSelection.js
+++ flash-front-end/src/EventSelection.js
@@ -25,7 +25,7 @@ const Vieweventdetailsforadmin = () => {
                 <div className=" container">
                     <div className="row">
                         <div className="col-6">
-                            <h3 className="mb-2">Photos </h3>
+                            <h3 className="mb-2">Events</h3>
                         </div>
 
 
@@ -95,40 +95,6 @@ const Vieweventdetailsforadmin = () => {
                 </div>
             </section>
 
-            <section className="pt-5 pb-5">
-                <div className=" container">
-                    <div className="row">
-
-                        <div className="col-12">
-                            <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
-                                <div className="carousel-inner">
-                                    <div className="carousel-item active">
-                                        <div className="row">
-                                            <div className="col-md-4 mb-3">
-                                                <div className="card border-dark border-5 rounded-3">
-                                                    <img className="img-fluid" alt="100%x280" src='https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp'></img>
-                                                </div>
-                                            </div>
-
-                                            <div className="col-md-4 mb-3">
-                                                <div className="card border-dark border-5 rounded-3">
-                                                    <img className="img-fluid" alt="100%x280"src='https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain1.webp'></img>
-                                                </div>
-                                            </div>
-
-                                            <div className="col-md-4 mb-3">
-                                                <div className="card border-dark border-5 rounded-3">
-                                                    <img className="img-fluid" alt="100%x280"src='https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain2.webp'></img>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
 
         </div>
 
