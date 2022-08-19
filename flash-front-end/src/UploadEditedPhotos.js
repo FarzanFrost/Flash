397c22d3c1ce0999226ed5a76f1fd7f8dcf29f68
rahuram <rahuram66@gmail.com>
2022-08-19 15:42:50 +0530

    adding drag box

--- flash-front-end/src/UploadEditedPhotos.js
+++ flash-front-end/src/UploadEditedPhotos.js
@@ -1,11 +1,8 @@
 import React from 'react'
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
-import sliverPhoto from './Images/silver-package.png'
-import goldPhoto from './Images/gold-package_.png'
-import diamondPhoto from './Images/diamond-package.png'
-import platinumPhoto from './Images/platinum-package.png'
-
+import './ViewPackages.css';
+//import './FileInput';
 
 const Vieweventdetailsforadmin = () => {
 
@@ -20,12 +17,27 @@ const Vieweventdetailsforadmin = () => {
     return (
 
         <div style={{...style1}}>
+            <section className="pt-5 pb-5">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-6">
+                            <form method="post" action="#" id="#">
+                                <div className="form-group files">
+                                    <label>Upload Your Images </label>
+                                    <input type="file" className="form-control" multiple=""></input>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
 
             <section className="pt-5 pb-5">
                 <div className=" container">
                     <div className="row">
                         <div className="col-6">
-                            <h3 className="mb-2">Photos </h3>
+                            <h3 className="mb-2">Upload Photos </h3>
                         </div>
 
 
@@ -36,31 +48,19 @@ const Vieweventdetailsforadmin = () => {
                                         <div className="row">
                                             <div className="col-md-4 mb-3">
                                                 <div className="card border-dark border-5 rounded-3">
-                                                    <img className="img-fluid" alt="100%x280" src='https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp'></img>
-                                                    <div className="card-body">
-                                                    <div className="card-body">
-                                                    </div>
-                                                </div>
+                                                    <img className="img-fluid" alt="100%x280" src='https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain3.webp'></img>
                                                 </div>
                                             </div>
 
                                             <div className="col-md-4 mb-3">
                                                 <div className="card border-dark border-5 rounded-3">
                                                     <img className="img-fluid" alt="100%x280"src='https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain1.webp'></img>
-                                                    <div className="card-body">
-                                                    <div className="card-body">
-                                                    </div>
-                                                </div>
                                                 </div>
                                             </div>
 
                                             <div className="col-md-4 mb-3">
                                                 <div className="card border-dark border-5 rounded-3">
                                                     <img className="img-fluid" alt="100%x280"src='https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain2.webp'></img>
-                                                    <div className="card-body">
-                                                    <div className="card-body">
-                                                    </div>
-                                                </div>
                                                 </div>
                                             </div>
                                         </div>
@@ -84,30 +84,18 @@ const Vieweventdetailsforadmin = () => {
                                             <div className="col-md-4 mb-3">
                                                 <div className="card border-dark border-5 rounded-3">
                                                     <img className="img-fluid" alt="100%x280" src='https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp'></img>
-                                                    <div className="card-body">
-                                                        <div className="card-body">
-                                                        </div>
-                                                    </div>
                                                 </div>
                                             </div>
 
                                             <div className="col-md-4 mb-3">
                                                 <div className="card border-dark border-5 rounded-3">
                                                     <img className="img-fluid" alt="100%x280"src='https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(18).webp'></img>
-                                                    <div className="card-body">
-                                                        <div className="card-body">
-                                                        </div>
-                                                    </div>
                                                 </div>
                                             </div>
 
                                             <div className="col-md-4 mb-3">
                                                 <div className="card border-dark border-5 rounded-3">
-                                                    <img className="img-fluid" alt="100%x280"src='https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain3.webp'></img>
-                                                    <div className="card-body">
-                                                        <div className="card-body">
-                                                        </div>
-                                                    </div>
+                                                    <img className="img-fluid" alt="100%x280"src='https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp'></img>
                                                 </div>
                                             </div>
                                         </div>
@@ -131,30 +119,18 @@ const Vieweventdetailsforadmin = () => {
                                             <div className="col-md-4 mb-3">
                                                 <div className="card border-dark border-5 rounded-3">
                                                     <img className="img-fluid" alt="100%x280" src='https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp'></img>
-                                                    <div className="card-body">
-                                                        <div className="card-body">
-                                                        </div>
-                                                    </div>
                                                 </div>
                                             </div>
 
                                             <div className="col-md-4 mb-3">
                                                 <div className="card border-dark border-5 rounded-3">
                                                     <img className="img-fluid" alt="100%x280"src='https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain1.webp'></img>
-                                                    <div className="card-body">
-                                                        <div className="card-body">
-                                                        </div>
-                                                    </div>
                                                 </div>
                                             </div>
 
                                             <div className="col-md-4 mb-3">
                                                 <div className="card border-dark border-5 rounded-3">
                                                     <img className="img-fluid" alt="100%x280"src='https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain2.webp'></img>
-                                                    <div className="card-body">
-                                                        <div className="card-body">
-                                                        </div>
-                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
