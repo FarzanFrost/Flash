1400fdfa1032d4c3948e3bf7357112da59baeb8f
Farzan <farzanroxz123@gmail.com>
2022-08-19 20:08:48 +0530

    created reviews entity and repository

---
+++ flash-back-end/src/main/java/com/example/flashbackend/Entity/Reviews.java
@@ -0,0 +1,71 @@
+package com.example.flashbackend.Entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.math.BigInteger;
+import java.sql.Timestamp;
+
+@Entity
+public class Reviews {
+
+    @Id
+    private BigInteger ReviewsID;
+
+    private String Comment;
+
+    private Timestamp DateTime;
+
+    private int Rate;
+
+    private boolean Deleted;
+
+    public Reviews(){}
+
+    public Reviews(BigInteger reviewsID, String comment, Timestamp dateTime, int rate, boolean deleted) {
+        ReviewsID = reviewsID;
+        Comment = comment;
+        DateTime = dateTime;
+        Rate = rate;
+        Deleted = deleted;
+    }
+
+    public BigInteger getReviewsID() {
+        return ReviewsID;
+    }
+
+    public void setReviewsID(BigInteger reviewsID) {
+        ReviewsID = reviewsID;
+    }
+
+    public String getComment() {
+        return Comment;
+    }
+
+    public void setComment(String comment) {
+        Comment = comment;
+    }
+
+    public Timestamp getDateTime() {
+        return DateTime;
+    }
+
+    public void setDateTime(Timestamp dateTime) {
+        DateTime = dateTime;
+    }
+
+    public int getRate() {
+        return Rate;
+    }
+
+    public void setRate(int rate) {
+        Rate = rate;
+    }
+
+    public boolean isDeleted() {
+        return Deleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        Deleted = deleted;
+    }
+}
