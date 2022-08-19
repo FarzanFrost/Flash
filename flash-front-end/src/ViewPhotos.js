a83784bd0273ab423d5d81a7496ed8f95e5eb49e
rahuram <rahuram66@gmail.com>
2022-08-19 13:20:34 +0530

    view all upload photos page compeled

--- flash-front-end/src/ViewPhotos.js
+++ flash-front-end/src/ViewPhotos.js
@@ -11,53 +11,15 @@ const Vieweventdetailsforadmin = () => {
 
     const style1 = {
 
-        backgroundColor: '#B36A5E',
-        height:'850px'
-    };
-
-    const style2 = {
-
-        width: '150px',
-        height: '550px',
         backgroundColor: '#767676',
+        height:'250px'
     };
 
-    const style3 = {
 
-        backgroundColor:'#4B2000',
-        color:'white'
-    };
 
     return (
 
         <div style={{...style1}}>
-
-
-            <div className=" row">
-                <div className="col-md-6">
-                    <div className="position-relative mx-5 m-2" style={{ width: '48rem',left:'16rem'}}>
-                        <input type="text" className="form-control" id="formGroupExampleInput" placeholder="Example input"></input>
-                    </div>
-                </div>
-
-
-
-                <div className="col-md-6" >
-
-                    <div className="dropdown position-relative mx-5 m-2" style={{ left:'16rem'}}>
-
-                        <select class="btn btn-secondary dropdown-toggle" type="button" id="Filter"
-                                data-bs-toggle="dropdown">
-                            <option value="All">All</option>
-                            <option value="eventname">Event Name</option>
-                            <option value="Date">Date</option>
-
-                        </select>
-                    </div>
-
-                </div>
-
-            </div>
 
             <section className="pt-5 pb-5">
                 <div className=" container">
@@ -67,93 +29,42 @@ const Vieweventdetailsforadmin = () => {
                         </div>
 
 
-
-
-                        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
-                                data-bs-slide="prev">
-                            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span className="visually-hidden">Previous</span>
-                        </button>
-                        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
-                                data-bs-slide="next">
-                            <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span className="visually-hidden">Next</span>
-                        </button>
-
-
                         <div className="col-12">
                             <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
-
                                 <div className="carousel-inner">
                                     <div className="carousel-item active">
                                         <div className="row">
-
                                             <div className="col-md-4 mb-3">
                                                 <div className="card border-dark border-5 rounded-3">
-                                                    <img className="img-fluid" alt="100%x280" src={sliverPhoto}></img>
+                                                    <img className="img-fluid" alt="100%x280" src='https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp'></img>
                                                     <div className="card-body">
                                                     <div className="card-body">
-
-
                                                     </div>
-
-
-                                                </div>
-
-                                                </div>
-                                            </div>
-
-                                            <div className="col-md-4 mb-3">
-                                                <div className="card border-dark border-5 rounded-3">
-                                                    <img className="img-fluid" alt="100%x280"src={goldPhoto}></img>
-                                                    <div className="card-body">
-                                                    <div className="card-body">
-
-
-                                                    </div>
-
-
                                                 </div>
                                                 </div>
                                             </div>
 
                                             <div className="col-md-4 mb-3">
                                                 <div className="card border-dark border-5 rounded-3">
-                                                    <img className="img-fluid" alt="100%x280"src={diamondPhoto}></img>
+                                                    <img className="img-fluid" alt="100%x280"src='https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain1.webp'></img>
                                                     <div className="card-body">
                                                     <div className="card-body">
-
-
                                                     </div>
+                                                </div>
+                                                </div>
+                                            </div>
 
-
+                                            <div className="col-md-4 mb-3">
+                                                <div className="card border-dark border-5 rounded-3">
+                                                    <img className="img-fluid" alt="100%x280"src='https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain2.webp'></img>
+                                                    <div className="card-body">
+                                                    <div className="card-body">
+                                                    </div>
                                                 </div>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
-
-
-                                    <div className="carousel-item">
-                                        <div className="col-md-4 mb-3">
-                                            <div className="card border-dark border-5 rounded-3">
-                                                <img className="img-fluid" alt="100%x280"src={platinumPhoto}></img>
-                                                <div className="card-body">
-                                                <div className="card-body">
-
-
-                                                </div>
-
-
-                                            </div>
-                                            </div>
-                                        </div>
-                                    </div>
-
-
-
-
-
                                 </div>
                             </div>
                         </div>
@@ -161,9 +72,99 @@ const Vieweventdetailsforadmin = () => {
                 </div>
             </section>
 
+            <section className="pt-5 pb-5">
+                <div className=" container">
+                    <div className="row">
 
+                        <div className="col-12">
+                            <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
+                                <div className="carousel-inner">
+                                    <div className="carousel-item active">
+                                        <div className="row">
+                                            <div className="col-md-4 mb-3">
+                                                <div className="card border-dark border-5 rounded-3">
+                                                    <img className="img-fluid" alt="100%x280" src='https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp'></img>
+                                                    <div className="card-body">
+                                                        <div className="card-body">
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
 
+                                            <div className="col-md-4 mb-3">
+                                                <div className="card border-dark border-5 rounded-3">
+                                                    <img className="img-fluid" alt="100%x280"src='https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(18).webp'></img>
+                                                    <div className="card-body">
+                                                        <div className="card-body">
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
 
+                                            <div className="col-md-4 mb-3">
+                                                <div className="card border-dark border-5 rounded-3">
+                                                    <img className="img-fluid" alt="100%x280"src='https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain3.webp'></img>
+                                                    <div className="card-body">
+                                                        <div className="card-body">
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section className="pt-5 pb-5">
+                <div className=" container">
+                    <div className="row">
+
+                        <div className="col-12">
+                            <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
+                                <div className="carousel-inner">
+                                    <div className="carousel-item active">
+                                        <div className="row">
+                                            <div className="col-md-4 mb-3">
+                                                <div className="card border-dark border-5 rounded-3">
+                                                    <img className="img-fluid" alt="100%x280" src='https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp'></img>
+                                                    <div className="card-body">
+                                                        <div className="card-body">
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div className="col-md-4 mb-3">
+                                                <div className="card border-dark border-5 rounded-3">
+                                                    <img className="img-fluid" alt="100%x280"src='https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain1.webp'></img>
+                                                    <div className="card-body">
+                                                        <div className="card-body">
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div className="col-md-4 mb-3">
+                                                <div className="card border-dark border-5 rounded-3">
+                                                    <img className="img-fluid" alt="100%x280"src='https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain2.webp'></img>
+                                                    <div className="card-body">
+                                                        <div className="card-body">
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
 
         </div>
 
