53a223c0898b96666c5975628de626b11dff9dfe
rahuram <rahuram66@gmail.com>
2022-08-23 10:13:51 +0530

    resturcture ViewPackagesWedding.js and remove buttons

--- flash-front-end/src/ViewPackagesWedding.js
+++ flash-front-end/src/ViewPackagesWedding.js
@@ -36,20 +36,6 @@ const Vieweventdetailsforadmin = () => {
                             <h3 className="mb-2">Packages </h3>
                         </div>
 
-
-
-                        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
-                                data-bs-slide="prev">
-                            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span className="visually-hidden">Previous</span>
-                        </button>
-                        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
-                                data-bs-slide="next">
-                            <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span className="visually-hidden">Next</span>
-                        </button>
-
-
                         <div className="col-12">
                             <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
 
@@ -73,7 +59,7 @@ const Vieweventdetailsforadmin = () => {
                                                             <h5>&nbsp;</h5>
                                                         </p>
                                                         <br/>
-                                                        <a href="#" className="btn" style={{...style3}}>Subscribe</a>
+
                                                     </div>
                                                 </div>
                                                 </div>
@@ -95,7 +81,7 @@ const Vieweventdetailsforadmin = () => {
                                                             <h5>Additional per sheet 2500</h5>
                                                         </p>
                                                         <br/>
-                                                        <a href="#" className="btn" style={{...style3}}>Subscribe</a>
+
                                                     </div>
                                                 </div>
                                                 </div>
@@ -118,47 +104,54 @@ const Vieweventdetailsforadmin = () => {
                                                             <h5>Additional per sheet 2500</h5>
                                                         </p>
                                                         <br/>
-                                                        <a href="#" className="btn" style={{...style3}}>Subscribe</a>
+
                                                     </div>
                                                 </div>
                                                 </div>
                                             </div>
 
                                         </div>
+
                                     </div>
-
-
-                                    <div className="carousel-item">
-                                        <div className="col-md-4 mb-3">
-                                            <div className="card border-dark border-5 rounded-3">
-                                                <img className="img-fluid" alt="100%x280"src={platinumPhoto}></img>
-                                                <div className="card-body">
-                                                <div className="card-body">
-                                                    <p className="card-text">
-                                                        <h4>Plantinum Package</h4>
-                                                        <h5>70page album</h5>
-                                                        <h5>Out Shoot</h5>
-                                                        <h5>Couple Frame(16*24)</h5>
-                                                        <h5>Signature Board</h5>
-                                                        <h5>highlights photo</h5>
-                                                        <h5>Save the Data(video or Photo)</h5>
-                                                        <h5>Additional per sheet 2500</h5>
-                                                    </p>
-                                                    <br/>
-                                                    <a href="#" className="btn" style={{...style3}}>Subscribe</a>
-                                                </div>
-                                            </div>
-                                            </div>
-                                        </div>
-                                    </div>
-
-
-
 
 
                                 </div>
                             </div>
                         </div>
+
+                        <div className="col-12">
+                            <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
+                                <div className="carousel-inner">
+                                    <div className="carousel-item active">
+                                        <div className="row">
+                                            <div className="col-md-4 mb-3">
+                                                <div className="card border-dark border-5 rounded-3">
+                                                    <img className="img-fluid" alt="100%x280" src={platinumPhoto}></img>
+                                                    <div className="card-body">
+                                                        <div className="card-body">
+
+                                                            <p className="card-text">
+                                                                <h4>Plantinum Package</h4>
+                                                                <h5>70page album</h5>
+                                                                <h5>Out Shoot</h5>
+                                                                <h5>Couple Frame(16*24)</h5>
+                                                                <h5>Signature Board</h5>
+                                                                <h5>highlights photo</h5>
+                                                                <h5>Save the Data(video or Photo)</h5>
+                                                                <h5>Additional per sheet 2500</h5>
+                                                            </p>
+                                                            <br/>
+
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
             </section>
