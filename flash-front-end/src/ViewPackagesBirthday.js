31035571d453773cd5d3808b3229e76a0657c3af
rahuram <rahuram66@gmail.com>
2022-08-23 07:38:45 +0530

    resturcture ViewPackagesBirthday.js and remove buttons

--- flash-front-end/src/ViewPackagesBirthday.js
+++ flash-front-end/src/ViewPackagesBirthday.js
@@ -37,21 +37,6 @@ const Vieweventdetailsforadmin = () => {
                             <h3 className="mb-2">Packages </h3>
                         </div>
 
-
-
-
-                        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
-                                data-bs-slide="prev">
-                            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span className="visually-hidden">Previous</span>
-                        </button>
-                        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
-                                data-bs-slide="next">
-                            <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span className="visually-hidden">Next</span>
-                        </button>
-
-
                         <div className="col-12">
                             <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
 
@@ -77,7 +62,7 @@ const Vieweventdetailsforadmin = () => {
                                                             <h5>&nbsp;</h5>
                                                         </p>
 
-                                                        <a href="#" className="btn" style={{...style3}}>Subscribe</a>
+
                                                     </div>
 
 
@@ -104,7 +89,7 @@ const Vieweventdetailsforadmin = () => {
                                                             <h5>&nbsp;</h5>
                                                         </p>
 
-                                                        <a href="#" className="btn" style={{...style3}}>Subscribe</a>
+
                                                     </div>
 
 
@@ -130,7 +115,7 @@ const Vieweventdetailsforadmin = () => {
                                                             <h5>calender</h5>
                                                         </p>
 
-                                                        <a href="#" className="btn" style={{...style3}}>Subscribe</a>
+
                                                     </div>
 
 
@@ -139,43 +124,46 @@ const Vieweventdetailsforadmin = () => {
                                             </div>
                                         </div>
                                     </div>
-
-
-                                    <div className="carousel-item">
-                                        <div className="col-md-4 mb-3">
-                                            <div className="card border-dark border-5 rounded-3">
-                                                <img className="img-fluid" alt="100%x280"src={platinumPhoto}></img>
-                                                <div className="card-body">
-                                                <div className="card-body">
-
-                                                    <p className="card-text">
-                                                        <h4>Plantinum Package</h4>
-                                                        <h5>60minuts Session</h5>
-                                                        <h5>for only current location</h5>
-                                                        <h5>outfit changes</h5>
-                                                        <h5>40 Images album</h5>
-                                                        <h5>printed frame(4*6)</h5>
-                                                        <h5>DVD slideshow</h5>
-                                                        <h5>out door shot(UnLimited hours)</h5>
-                                                        <h5>calender</h5>
-                                                    </p>
-
-                                                    <a href="#" className="btn" style={{...style3}}>Subscribe</a>
-                                                </div>
-
-
-                                            </div>
-                                            </div>
-                                        </div>
-                                    </div>
-
-
-
-
-
                                 </div>
                             </div>
                         </div>
+
+                        <div className="col-12">
+                            <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
+                                <div className="carousel-inner">
+                                    <div className="carousel-item active">
+                                        <div className="row">
+                                            <div className="col-md-4 mb-3">
+                                                <div className="card border-dark border-5 rounded-3">
+                                                    <img className="img-fluid" alt="100%x280" src={platinumPhoto}></img>
+                                                    <div className="card-body">
+                                                        <div className="card-body">
+
+                                                            <p className="card-text">
+                                                                <h4>Plantinum Package</h4>
+                                                                <h5>60minuts Session</h5>
+                                                                <h5>for only current location</h5>
+                                                                <h5>outfit changes</h5>
+                                                                <h5>40 Images album</h5>
+                                                                <h5>printed frame(4*6)</h5>
+                                                                <h5>DVD slideshow</h5>
+                                                                <h5>out door shot(UnLimited hours)</h5>
+                                                                <h5>calender</h5>
+                                                            </p>
+                                                            <br/>
+
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+
                     </div>
                 </div>
             </section>
