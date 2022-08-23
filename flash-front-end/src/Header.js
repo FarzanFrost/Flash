5613e0cc6dce048b09973dc830ef15f211dddaef
Thineshkanth <thineshkanth3759@gmail.com>
2022-08-23 13:43:05 +0530

    final header page design

--- flash-front-end/src/Header.js
+++ flash-front-end/src/Header.js
@@ -12,51 +12,31 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 const Header = () => {
     return (
 
-        // <div className="navbar navbar-expand-lg navbar-dark bg-dark justify-content-between">
-        //
-        //     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
-        //             aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        //         <span className="navbar-toggler-icon"></span>
-        //     </button>
-        //     <div className="collapse navbar-collapse  " id="navbarNavAltMarkup">
-        //         <div className="navbar-nav">
-        //             <button className="btn btn-dark p-4">
-        //                 <i className="bi bi-list"></i>
-        //             </button>
-        //             <img src={FlashLogo} alt="logo" height="80px" width="80px"/>
-        //             <a className="nav-item nav-link active  m-3 pb-2 pe-5 ps-lg-5 " href="#" >Home <span className="sr-only"></span></a>
-        //             <a className="nav-item nav-link  m-3 pb-2 pe-5 ps-5" href="#">Book Now</a>
-        //             <a className="nav-item nav-link  m-3 pb-2 pe-5 ps-5  " style={{marginRight:'40rem'}} href="#">Sign Out</a>
-        //
-        //
-        //     </div>
-        // </div>
-        // </div>
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-            <Container>
+        <div className="navbar navbar-expand-lg navbar-dark bg-dark justify-content-between">
 
-                <button className="btn btn-dark p-4">
-                    <i className="bi bi-list"></i>
-                </button>
-                <Navbar.Brand href="#home">
-                    <img src={FlashLogo} alt="logo" height="70px" width="70px"/>
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="me-auto">
-                        {/*<Nav.Link href="#Home">Home</Nav.Link>*/}
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+                    aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse  " id="navbarNavAltMarkup">
+                <div className="navbar-nav">
+                    <button className="btn btn-dark p-4">
+                        <i className="bi bi-list"></i>
+                    </button>
+                    <img src={FlashLogo} alt="logo" height="80px" width="80px"/>
+                    <a className="nav-item nav-link active  m-3 pb-2 pe-5 ps-lg-5  " href="#" > <span className="sr-only"></span></a>
+                    <a className="nav-item nav-link active  m-3 pb-2 pe-5 ps-lg-5  " href="#" > <span className="sr-only"></span></a>
+                    <a className="nav-item nav-link active  m-3 pb-2 pe-5 ps-lg-5  " href="#" > <span className="sr-only"></span></a>
+                    <a className="nav-item nav-link active  m-3 pb-2 pe-5 ps-lg-5  " href="#" > <span className="sr-only"></span></a>
+                    <a className="nav-item nav-link active  m-3 pb-2 pe-5 ps-lg-5  " href="#" > <span className="sr-only"></span></a>
+                    <a className="nav-item nav-link active  m-3 pb-2 pe-5 ps-lg-5  " href="#" > <span className="sr-only"></span></a>
+                        <a className="nav-item nav-link active  m-3 pb-2 pe-5 ps-lg-5  me-0" href="#" >Home <span className="sr-only"></span></a>
+                        <a className="nav-item nav-link  m-3 pb-2 pe-5 ps-5 me-0" href="#">Book Now</a>
+                        <a className="nav-item nav-link  m-3 pb-2 pe-5 ps-5  me-0"  href="#">Sign Out</a>
 
-
-                    </Nav>
-                    <Nav>
-                        <Nav.Link href="#Home">Home</Nav.Link>
-                        <Nav.Link href="#SignOut">
-                            SignOut
-                        </Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+            </div>
+        </div>
+        </div>
 
     )
 }
--- flash-front-end/src/UIStructure/Admin.js
+++ flash-front-end/src/UIStructure/Admin.js
@@ -3,6 +3,8 @@ import Footer from "../Footer";
 import FlashLogo from "../Images/FlashLogo.png";
 import MoreDetailsOfEventsForAdmin from "../MoreDetailsOfEventsForAdmin";
 import Vieweventdetailsforadmin from "../Vieweventdetailsforadmin";
+import AdminViewAllReviews from "../AdminViewAllReviews";
+import AdminViewEmployeeDetailspage from "../AdminViewEmployeeDetailspage";
 
 
 const Customer = () => {
@@ -30,22 +32,27 @@ const Customer = () => {
         <div>
 
             {/*header begins*/}
-            <div className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="navbar navbar-expand-lg navbar-dark bg-dark justify-content-between">
 
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
                         aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div className="collapse navbar-collapse  " id="navbarNavAltMarkup">
                     <div className="navbar-nav">
-                        <button className="btn btn-dark px-3 ms-1" onClick={showHideSideNav}>
+                        <button className="btn btn-dark p-4">
                             <i className="bi bi-list"></i>
                         </button>
                         <img src={FlashLogo} alt="logo" height="80px" width="80px"/>
-                        <a className="nav-item nav-link active  m-3 pb-2 pe-5 ps-5" href="#" >Home <span className="sr-only"></span></a>
-                        <a className="nav-item nav-link  m-3 pb-2 pe-5 ps-5" href="#">Book Now</a>
-                        <a className="nav-item nav-link  m-3 pb-2 pe-5 ps-5" href="#">Sign Out</a>
-
+                        <a className="nav-item nav-link active  m-3 pb-2 pe-5 ps-lg-5  " href="#" > <span className="sr-only"></span></a>
+                        <a className="nav-item nav-link active  m-3 pb-2 pe-5 ps-lg-5  " href="#" > <span className="sr-only"></span></a>
+                        <a className="nav-item nav-link active  m-3 pb-2 pe-5 ps-lg-5  " href="#" > <span className="sr-only"></span></a>
+                        <a className="nav-item nav-link active  m-3 pb-2 pe-5 ps-lg-5  " href="#" > <span className="sr-only"></span></a>
+                        <a className="nav-item nav-link active  m-3 pb-2 pe-5 ps-lg-5  " href="#" > <span className="sr-only"></span></a>
+                        <a className="nav-item nav-link active  m-3 pb-2 pe-5 ps-lg-5  " href="#" > <span className="sr-only"></span></a>
+                        <a className="nav-item nav-link active  m-3 pb-2 pe-5 ps-lg-5  me-0" href="#" >Home <span className="sr-only"></span></a>
+                        <a className="nav-item nav-link  m-3 pb-2 pe-5 ps-5 me-0" href="#">Book Now</a>
+                        <a className="nav-item nav-link  m-3 pb-2 pe-5 ps-5  me-0"  href="#">Sign Out</a>
 
                     </div>
                 </div>
@@ -121,11 +128,6 @@ const Customer = () => {
                 <div className={ isSideNavVisible ? "col-10" : "col-12" }>
                     <Vieweventdetailsforadmin/>
                     <MoreDetailsOfEventsForAdmin/>
-
-
-
-
-
                 </div>
 
             </div>
