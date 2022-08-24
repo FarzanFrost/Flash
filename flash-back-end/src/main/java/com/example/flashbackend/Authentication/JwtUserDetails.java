ae24ba036add76b9289c7611a4dcc1f6fde93478
Farzan <farzanroxz123@gmail.com>
2022-08-24 18:53:33 +0530

    adjusted jwt authentication to support user in database

--- flash-back-end/src/main/java/com/example/flashbackend/Authentication/JwtUserDetails.java
+++ flash-back-end/src/main/java/com/example/flashbackend/Authentication/JwtUserDetails.java
@@ -1,10 +1,12 @@
 package com.example.flashbackend.Authentication;
 
+import com.example.flashbackend.Entity.Login;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -13,12 +15,25 @@ public class JwtUserDetails implements UserDetails {
 
   private static final long serialVersionUID = 5155720064139820502L;
 
-  private final Long id;
+  private final BigInteger id;
   private final String username;
   private final String password;
   private final Collection<? extends GrantedAuthority> authorities;
 
-  public JwtUserDetails(Long id, String username, String password, String role) {
+  public JwtUserDetails(Login login){
+
+    id = login.getCustomer().getCustomerID();
+    username = login.getEmail();
+    password = login.getPassword();
+
+    List<SimpleGrantedAuthority> authorities = new ArrayList<SimpleGrantedAuthority>();
+    authorities.add(new SimpleGrantedAuthority( "Customer" ));
+
+    this.authorities = authorities;
+
+  }
+
+  public JwtUserDetails(BigInteger id, String username, String password, String role) {
     this.id = id;
     this.username = username;
     this.password = password;
@@ -30,7 +45,7 @@ public class JwtUserDetails implements UserDetails {
   }
 
   @JsonIgnore
-  public Long getId() {
+  public BigInteger getId() {
     return id;
   }
 
