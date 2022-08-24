31c01df7f06a912531435fa621b25dc8c285b63e
Farzan <farzanroxz123@gmail.com>
2022-08-24 18:59:26 +0530

    created authentication context provider to handle all login, signup, and user details

--- flash-front-end/src/ContextFiles/Authentication/AuthenticationContextProvider.js
+++ flash-front-end/src/ContextFiles/Authentication/AuthenticationContextProvider.js
@@ -5,7 +5,11 @@ export const AuthenticationContext = createContext( undefined )
 
 const AuthenticationContextProvider = ( props ) => {
 
+    const serverLink = 'http://localhost:8080'
+
     const [ authenticated , setAuthenticated ] = useState( true )
+
+    const [ userDetailsAfterAuthentication , setUserDetailsAfterAuthentication ] = useState( null )
 
     const authenticateUser = () => {
 
@@ -13,11 +17,34 @@ const AuthenticationContextProvider = ( props ) => {
 
     }
 
+    const signUp = ( data ) => {
+
+        axios.post( serverLink + '/createUser' , data ).then(
+
+            ( response ) => {
+
+                console.log( response.data )
+
+                login( data.email , data.password )
+
+            }
+
+        ).catch(
+
+            () => { alert( "Error!!! 3") }
+
+        )
+
+
+
+    }
+
+    /*Here the username refers the email*/
     const login = ( username , password ) => {
 
         axios.post(
 
-            'http://localhost:8080/authenticate',{
+            serverLink+'/authenticate',{
                 username,
                 password
             }).then(
@@ -25,6 +52,23 @@ const AuthenticationContextProvider = ( props ) => {
             ( response ) => {
 
                 storeSessionJWT( username , response.data.token )
+                setAuthenticated( true )
+                getUserDetailsAfterAuthenticated( username )
+                /*axios.get( 'http://localhost:8080/Greetings' ).then(
+
+                    ( response ) => {
+
+                        console.log( response.data )
+
+                    }
+
+                ).catch(
+
+                    () => {
+
+                        alert( "ERROR!!! 2" )
+
+                    })*/
 
             }).catch(
 
@@ -34,7 +78,25 @@ const AuthenticationContextProvider = ( props ) => {
 
             })
 
-        // console.log( username,password)
+    }
+
+    const getUserDetailsAfterAuthenticated = ( email ) => {
+
+        axios.get( serverLink + '/getUserDetails/' + email ).then(
+
+            ( response ) => { /*console.log(response.data);*/setUserDetailsAfterAuthentication( response.data );
+                window.history.pushState(null, null, "/Customer");window.location.reload()
+            }
+
+        ).catch(
+
+            () => {
+
+                alert( "Error!!! 4" )
+
+            }
+
+        )
 
     }
 
@@ -73,12 +135,13 @@ const AuthenticationContextProvider = ( props ) => {
     const logout = () => {
 
         sessionStorage.removeItem( 'authenticatedUser' );
+        setAuthenticated( false );
 
     }
 
     return(
 
-        <AuthenticationContext.Provider value={ { authenticated , authenticateUser , login } }>
+        <AuthenticationContext.Provider value={ { authenticated , authenticateUser , login , signUp } }>
 
             { props.children }
 
