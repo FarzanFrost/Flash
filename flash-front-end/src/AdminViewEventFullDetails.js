6056d66d1ac29067113e9b991487ed82886cf2a8
Thineshkanth <thineshkanth3759@gmail.com>
2022-08-29 07:25:18 +0530

    add imports for adminviewfulldetails page

---
+++ flash-front-end/src/AdminViewEventFullDetails.js
@@ -0,0 +1,7 @@
+import  React from 'react'
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import loginPhoto from "./Images/backgroudphoto.jpg";
+import { useState } from 'react';
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
