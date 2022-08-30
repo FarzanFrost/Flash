aab420b7d3e7c5ca0163878b1e02fb81db95d854
nitharsha <nitharshathev@gmail.com>
2022-08-30 12:57:19 +0530

    set the customer profile page with navigations

--- flash-front-end/src/CustomerProfile.js
+++ flash-front-end/src/CustomerProfile.js
@@ -4,15 +4,15 @@ import {Form} from "react-bootstrap";
 
 const CustomerProfile = () =>{
     return(
-        <div className="container h-100 row d-flex justify-content-center align-items-center h-100 col-12 ">
+        <div className="h-100">
 
-            {/*<section className="h-100">*/}
+            <section className="h-100">
 
-                {/*<div className="container py-0 h-100">*/}
+                <div className="container py-5 h-100">
 
-                    {/*<div className="row d-flex justify-content-center align-items-center h-100 bg-danger col-12">*/}
+                    <div className="row d-flex justify-content-center align-items-center h-100">
 
-                        {/*<div className="col-12 bg-secondary">*/}
+                        <div className="col-xl-11 my-5 py-2 my-lg-1 py-lg-1 my-xl-0 py-xl-0">
 
                             <div className="card rounded-3 text-black my-lg-0 my-xl-0 py-xl-0" style={ { backgroundColor : '#d7d7d7' } }>
 
@@ -155,13 +155,13 @@ const CustomerProfile = () =>{
 
                             </div>
 
-                        {/*</div>*/}
+                        </div>
 
-                    {/*</div>*/}
+                    </div>
 
-                {/*</div>*/}
+                </div>
 
-            {/*</section>*/}
+            </section>
 
         </div>
     )
