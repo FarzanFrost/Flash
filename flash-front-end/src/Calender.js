ec391b282606217421f04e0cfefa10f1b47dd69f
Thineshkanth <thineshkanth3759@gmail.com>
2022-09-01 14:35:27 +0530

    add a calender.In here just add  a frame for calender

--- flash-front-end/src/Calender.js
+++ flash-front-end/src/Calender.js
@@ -1,40 +1,18 @@
 import  React from 'react'
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
-import calender from "./Images/calender.png";
-import { useState } from 'react';
+import  { useState } from 'react';
+import Calendar from 'react-calendar';
 
 
 const Calender = () => {
-    const [show, setShow] = useState(false);
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+        const [value, onChange] = useState(new Date());
 
-    return (
-
-        <div className="h-100">
-
-            <div className='container'>
-
-                <div className="pt-4" >
-
-                    <div className="card border-dark border-0 rounded-5 shadow mt-3 mx-5 " style={{backgroundColor: '#d7d7d7'}}>
-
-                        <h2 className="text-center mt-3" >Calender</h2>
-
-
-                        <img src={ calender } className="img-fluid rounded-3 h-100" alt="Booking"/>
-
-
-                    </div>
-                </div>
-
+        return (
+            <div>
+                <Calendar onChange={onChange} value={value} />
             </div>
-
-        </div>
-
-
 
     )
 
