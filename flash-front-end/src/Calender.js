892fc59c4dc460ef7bb12a117cae8393d9d1c4a6
Thineshkanth <thineshkanth3759@gmail.com>
2022-09-01 14:38:58 +0530

    add another new calender depencies

--- flash-front-end/src/Calender.js
+++ flash-front-end/src/Calender.js
@@ -3,6 +3,7 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import  { useState } from 'react';
 import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
 
 const Calender = () => {
