e88eff97c149986bcafac7f9fc5b8b801d89a26d
Farzan <farzanroxz123@gmail.com>
2022-09-01 14:57:28 +0530

    created manager get count

--- flash-back-end/src/main/java/com/example/flashbackend/Controller/Manager.java
+++ flash-back-end/src/main/java/com/example/flashbackend/Controller/Manager.java
@@ -1,12 +1,23 @@
 package com.example.flashbackend.Controller;
 
+import com.example.flashbackend.DAO.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin( origins = "http://localhost:3000")
 public class Manager {
 
-    
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+    @GetMapping("/employeeCount")
+    public long getEmployeeCount(){
+
+        return employeeRepository.count();
+
+    }
 
 }
