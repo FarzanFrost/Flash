5e2810ba1e11cc8d5dcb2056e8e0749a674805f9
Farzan <farzanroxz123@gmail.com>
2022-09-01 15:45:59 +0530

    added functions to get employer count, event count, customer count, package list

--- flash-back-end/src/main/java/com/example/flashbackend/Component/ManagerComponent.java
+++ flash-back-end/src/main/java/com/example/flashbackend/Component/ManagerComponent.java
@@ -1,8 +1,14 @@
 package com.example.flashbackend.Component;
 
+import com.example.flashbackend.DAO.CustomerRepository;
 import com.example.flashbackend.DAO.EmployeeRepository;
+import com.example.flashbackend.DAO.EventRepository;
+import com.example.flashbackend.DAO.PackageRepository;
+import com.example.flashbackend.Entity.Package;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class ManagerComponent {
@@ -10,9 +16,36 @@ public class ManagerComponent {
     @Autowired
     EmployeeRepository employeeRepository;
 
+    @Autowired
+    EventRepository eventRepository;
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Autowired
+    PackageRepository packageRepository;
+
     public long getEmployeeCount(){
 
         return employeeRepository.count();
+
+    }
+
+    public long getEventCount(){
+
+        return eventRepository.count();
+
+    }
+
+    public long getCustomerCount(){
+
+        return customerRepository.count();
+
+    }
+
+    public List<Package> getPackages(){
+
+        return packageRepository.findAll();
 
     }
 
--- flash-back-end/src/main/java/com/example/flashbackend/Controller/Manager.java
+++ flash-back-end/src/main/java/com/example/flashbackend/Controller/Manager.java
@@ -1,10 +1,13 @@
 package com.example.flashbackend.Controller;
 
 import com.example.flashbackend.Component.ManagerComponent;
+import com.example.flashbackend.Entity.Package;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin( origins = "http://localhost:3000")
@@ -17,6 +20,27 @@ public class Manager {
     public long getEmployeeCount(){
 
         return managerComponent.getEmployeeCount();
+
+    }
+
+    @GetMapping("/eventCount")
+    public long getEventCount(){
+
+        return managerComponent.getEventCount();
+
+    }
+
+    @GetMapping("/customerCount")
+    public long getCustomerCount(){
+
+        return managerComponent.getCustomerCount();
+
+    }
+
+    @GetMapping("/getPackages")
+    public List<Package> getpackages(){
+
+        return managerComponent.getPackages();
 
     }
 
