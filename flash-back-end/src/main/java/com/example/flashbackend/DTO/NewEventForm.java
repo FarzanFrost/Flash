51fea604b050dc8b8be79dcffd38e2527d74cc75
nitharsha <nitharshathev@gmail.com>
2022-09-05 17:04:55 +0530

    add define the all fields in newEventForm

--- flash-back-end/src/main/java/com/example/flashbackend/DTO/NewEventForm.java
+++ flash-back-end/src/main/java/com/example/flashbackend/DTO/NewEventForm.java
@@ -1,4 +1,30 @@
 package com.example.flashbackend.DTO;
 
+import java.sql.Date;
+import java.sql.Time;
+
 public class NewEventForm {
+
+    private String event;
+
+    private Date date;
+
+    private Time startTime;
+
+    private Time endTime;
+
+    private String address;
+
+    private String packageName;
+    public NewEventForm(){}
+
+    public NewEventForm(String event, Date date, Time startTime, Time endTime, String address, String packageName) {
+        this.event = event;
+        this.date = date;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.address = address;
+        this.packageName = packageName;
+    }
+
 }
