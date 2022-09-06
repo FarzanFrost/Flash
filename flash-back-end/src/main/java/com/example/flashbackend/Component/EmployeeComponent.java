f6e8ae6549beca4fc6c69b433e2495dd640e7c96
Thineshkanth <thineshkanth3759@gmail.com>
2022-09-06 13:53:13 +0530

    create a eventrespository class for employee component file

--- flash-back-end/src/main/java/com/example/flashbackend/Component/EmployeeComponent.java
+++ flash-back-end/src/main/java/com/example/flashbackend/Component/EmployeeComponent.java
@@ -1,5 +1,18 @@
 package com.example.flashbackend.Component;
+import com.example.flashbackend.DAO.EventRepository;
+import com.example.flashbackend.Entity.Event;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
 
 public class EmployeeComponent {
-    
+
+    @Autowired
+    EventRepository eventRepository;
+
+    public List<Event>getEvents(){
+        return eventRepository.findAll();
+    }
+
 }
