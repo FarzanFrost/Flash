2e9215ead705a2c85f7d7e83c06f56efb6c4baf9
Thineshkanth <thineshkanth3759@gmail.com>
2022-09-06 14:37:46 +0530

    create a code for view all events in employee controller

--- flash-back-end/src/main/java/com/example/flashbackend/Component/EmployeeComponent.java
+++ flash-back-end/src/main/java/com/example/flashbackend/Component/EmployeeComponent.java
@@ -2,10 +2,11 @@ package com.example.flashbackend.Component;
 import com.example.flashbackend.DAO.EventRepository;
 import com.example.flashbackend.Entity.Event;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-
+@Component
 public class EmployeeComponent {
 
     @Autowired
--- flash-back-end/src/main/java/com/example/flashbackend/Controller/Employee.java
+++ flash-back-end/src/main/java/com/example/flashbackend/Controller/Employee.java
@@ -1,13 +1,26 @@
 package com.example.flashbackend.Controller;
 
-
+import com.example.flashbackend.Component.EmployeeComponent;
+import com.example.flashbackend.Entity.Event;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
 public class Employee {
 
+    @Autowired
+    EmployeeComponent employeeComponent;
 
+    @GetMapping("/events")
+    public List<Event>getEvents(){
+        return employeeComponent.getEvents();
+    }
 
 }
+
+
