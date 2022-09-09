4091a25292e54e318cd4e887c8bce1d692d868a6
nitharsha <nitharshathev@gmail.com>
2022-09-09 16:41:34 +0530

    get all booking events

--- flash-back-end/src/main/java/com/example/flashbackend/Controller/Customer.java
+++ flash-back-end/src/main/java/com/example/flashbackend/Controller/Customer.java
@@ -1,12 +1,22 @@
 package com.example.flashbackend.Controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
+import com.example.flashbackend.Component.CustomerComponent;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.example.flashbackend.Entity.Event;
+import org.w3c.dom.stylesheets.LinkStyle;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin( origins = "http://localhost:3000")
 public class Customer {
 
-//    @PostMapping("/newEvent")
+//    @GetMapping("/getEvents")
+    @GetMapping("/Events")
+
+    public List<Event> getEvents(){
+        return CustomerComponent.getEvents();
+    }
 }
