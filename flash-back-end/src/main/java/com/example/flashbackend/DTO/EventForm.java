a195cd6bb0742be243213ed65ba767a4fcdcde82
nitharsha <nitharshathev@gmail.com>
2022-10-18 17:16:52 +0530

    create a event attributes

--- flash-back-end/src/main/java/com/example/flashbackend/DTO/EventForm.java
+++ flash-back-end/src/main/java/com/example/flashbackend/DTO/EventForm.java
@@ -6,29 +6,29 @@ import java.util.Date;
 
 public class EventForm {
 
-//    private Date eventDate;
-//
-//    private Time startTime;
-//
-//    private Time endTime;
-//
-//    private String status;
-//
-//    private Float advancedAmount;
-//
-//    private BigInteger customerId;
-//
-//    private BigInteger packageId;
-//
-//    private String address;
-//
-//    private String latitude;
-//
-//    private String longitude;
-//
-//    private Integer extraPage;
-//
-//    private String category;
-//
-//    private Integer delivered;
+    private Date eventDate;
+
+    private Time startTime;
+
+    private Time endTime;
+
+    private String status;
+
+    private Float advancedAmount;
+
+    private BigInteger customerId;
+
+    private BigInteger packageId;
+
+    private String address;
+
+    private String latitude;
+
+    private String longitude;
+
+    private Integer extraPage;
+
+    private String category;
+
+    private Integer delivered;
 }
