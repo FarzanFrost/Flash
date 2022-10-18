2ff59cc5636531785d6b2857474d2468c9efd788
nitharsha <nitharshathev@gmail.com>
2022-10-18 15:49:58 +0530

    find all from customer in customer repository

--- flash-back-end/src/main/java/com/example/flashbackend/DAO/CustomerRepository.java
+++ flash-back-end/src/main/java/com/example/flashbackend/DAO/CustomerRepository.java
@@ -6,12 +6,15 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.math.BigInteger;
 import java.util.List;
+import java.util.Optional;
 
 public interface CustomerRepository extends CrudRepository< Customer , BigInteger > {
 
     @Override
     List<Customer> findAll();
 
+//    @Override
+//    Optional<Customer> findByCustomerID(1) ;
 //    Customer findByLogin(Login login);
 
 }
