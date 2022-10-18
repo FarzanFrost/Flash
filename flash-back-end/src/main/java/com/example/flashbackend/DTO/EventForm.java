04e3825017c729186f22a565b97b391458bc15fd
nitharsha <nitharshathev@gmail.com>
2022-10-18 15:52:33 +0530

    create a new DTO file for insert a new event booking

---
+++ flash-back-end/src/main/java/com/example/flashbackend/DTO/EventForm.java
@@ -0,0 +1,34 @@
+package com.example.flashbackend.DTO;
+
+import java.math.BigInteger;
+import java.sql.Time;
+import java.util.Date;
+
+public class EventForm {
+
+//    private Date eventDate;
+//
+//    private Time startTime;
+//
+//    private Time endTime;
+//
+//    private String status;
+//
+//    private Float advancedAmount;
+//
+//    private BigInteger customerId;
+//
+//    private BigInteger packageId;
+//
+//    private String address;
+//
+//    private String latitude;
+//
+//    private String longitude;
+//
+//    private Integer extraPage;
+//
+//    private String category;
+//
+//    private Integer delivered;
+}
