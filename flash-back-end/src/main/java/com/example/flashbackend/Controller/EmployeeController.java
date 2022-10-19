b340d8f7b3b9c6d1df02e78bc48156020f827023
Farzan <farzanroxz123@gmail.com>
2022-10-19 15:54:10 +0530

    corrected some errors affter merging

--- flash-back-end/src/main/java/com/example/flashbackend/Controller/EmployeeController.java
+++ flash-back-end/src/main/java/com/example/flashbackend/Controller/EmployeeController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
-public class Employee {
+public class EmployeeController {
 
     @Autowired
     EmployeeComponent employeeComponent;
