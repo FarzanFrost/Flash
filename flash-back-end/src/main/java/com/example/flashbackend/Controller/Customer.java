55b052e8c1a4947d3bca625e9512665ede3c5164
nitharsha <nitharshathev@gmail.com>
2022-10-22 21:35:51 +0530

    add the method for create a new event

--- flash-back-end/src/main/java/com/example/flashbackend/Controller/Customer.java
+++ flash-back-end/src/main/java/com/example/flashbackend/Controller/Customer.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import com.example.flashbackend.Component.CustomerComponent;
 import com.example.flashbackend.Entity.Event;
+import com.example.flashbackend.Entity.Customer;
+import com.example.flashbackend.Entity.Package;
 
 import java.util.List;
 
@@ -33,12 +35,14 @@ public class Customer {
         return customerComponent.getReviews();
     }
 
-//    @PostMapping("/NewEvent")
-//    public String addEvent(@RequestBody AddEvent addEvent){
-//        //String status, String advanceAmount, Date eventDate, Time startTime, Time endTime, String address, String latitude, String longtitude, Integer extraPage, String category, boolean delivered
-//        Event event = new Event(  addEvent.getStatus(), addEvent.getAdvancedAmount(), addEvent.getEventDate(), addEvent.getStartTime(), addEvent.getEndTime(), addEvent.getAddress(), addEvent.getCustomerId(), addEvent.getPackageId(),  addEvent.getLatitude(), addEvent.getLongitude(), addEvent.getExtraPage(), addEvent.getCategory(), addEvent.getDelivered() );
-//
-//        return customerComponent.addNewEvent( event );
-//    }
+    @PostMapping("/NewEvent")
+    public String addEvent(@RequestBody AddEvent addEvent){
+        //String status, String advanceAmount, Date eventDate, Time startTime, Time endTime, String address, String latitude, String longtitude, Integer extraPage, String category, boolean delivered
+        Event event = new Event(  addEvent.getStatus(), addEvent.getAdvancedAmount(), addEvent.getEventDate(), addEvent.getStartTime(), addEvent.getEndTime(), addEvent.getAddress(), addEvent.getLatitude(), addEvent.getLongitude(), addEvent.getExtraPage(), addEvent.getCategory(), addEvent.getDelivered() );
+        Customer customer = new Customer(addEvent.getCustomerId());
+        Package packages = new Package(addEvent.getPackageId());
+
+        return customerComponent.addNewEvent( event, customer, packages );
+    }
 
 }
