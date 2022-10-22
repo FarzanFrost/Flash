1cae0be8d12da4c0cac1386eb465b1d987a02bb4
Afrah <afrahnazeem@gmail.com>
2022-10-22 17:47:18 +0530

    view review for admin

--- flash-back-end/src/main/java/com/example/flashbackend/Controller/Admin.java
+++ flash-back-end/src/main/java/com/example/flashbackend/Controller/Admin.java
@@ -1,4 +1,15 @@
 package com.example.flashbackend.Controller;
 
+import com.example.flashbackend.Component.ManagerComponent;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class Admin {
+
+    @Autowired
+    ManagerComponent managerComponent;
+
+    @GetMapping("/Reviews")
+    public List<Reviews> getReviews(){
+        return managerComponent.getReviws();
+    }
 }
