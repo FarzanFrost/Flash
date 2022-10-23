05eb591a3f018a68c90455f30d84785eb15c3128
nitharsha <nitharshathev@gmail.com>
2022-10-23 09:28:46 +0530

    change the name of customer controller

--- flash-back-end/src/main/java/com/example/flashbackend/Controller/CustomerController.java
+++ flash-back-end/src/main/java/com/example/flashbackend/Controller/CustomerController.java
@@ -7,8 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import com.example.flashbackend.Component.CustomerComponent;
 import com.example.flashbackend.Entity.Event;
-import com.example.flashbackend.Entity.Customer;
-import com.example.flashbackend.Entity.Package;
+
+
 
 import java.util.List;
 
@@ -39,7 +39,9 @@ public class Customer {
     public String addEvent(@RequestBody AddEvent addEvent){
         //String status, String advanceAmount, Date eventDate, Time startTime, Time endTime, String address, String latitude, String longtitude, Integer extraPage, String category, boolean delivered
         Event event = new Event(  addEvent.getStatus(), addEvent.getAdvancedAmount(), addEvent.getEventDate(), addEvent.getStartTime(), addEvent.getEndTime(), addEvent.getAddress(), addEvent.getLatitude(), addEvent.getLongitude(), addEvent.getExtraPage(), addEvent.getCategory(), addEvent.getDelivered() );
+
         Customer customer = new Customer(addEvent.getCustomerId());
+
         Package packages = new Package(addEvent.getPackageId());
 
         return customerComponent.addNewEvent( event, customer, packages );
