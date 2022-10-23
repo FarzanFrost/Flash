789dc6b0eae1a800b487ea52b9ad242b52a70352
nitharsha <nitharshathev@gmail.com>
2022-10-23 11:40:03 +0530

    create all attributes for reviews

--- flash-back-end/src/main/java/com/example/flashbackend/DTO/AddReview.java
+++ flash-back-end/src/main/java/com/example/flashbackend/DTO/AddReview.java
@@ -1,4 +1,14 @@
 package com.example.flashbackend.DTO;
 
+import java.sql.Timestamp;
+
 public class AddReview {
+
+    private String comment;
+
+    private Timestamp dateTime;
+
+    private Integer rate;
+
+    private Boolean deleted;
 }
