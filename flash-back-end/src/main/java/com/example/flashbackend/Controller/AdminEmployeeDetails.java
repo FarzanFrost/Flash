061de4d00b66c04ca7c398740558aa9707e366f2
SrimohanRahuram <rahuram66@gmail.com>
2022-10-22 17:52:31 -0800

    adding packages admindetailfuntion in controller

--- flash-back-end/src/main/java/com/example/flashbackend/Controller/AdminEmployeeDetails.java
+++ flash-back-end/src/main/java/com/example/flashbackend/Controller/AdminEmployeeDetails.java
@@ -1,4 +1,19 @@
 package com.example.flashbackend.Controller;
 
+
+import com.example.flashbackend.Component.ManagerComponent;
+import com.example.flashbackend.DTO.AddEmployee;
+import com.example.flashbackend.Entity.*;
+import com.example.flashbackend.Entity.Package;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigInteger;
+import java.sql.Timestamp;
+import java.util.List;
+
+@RestController
+@CrossOrigin( origins = "http://localhost:3000")
 public class AdminEmployeeDetails {
+
 }
