b81b2343546a165bd6a52d8dfd54af5ce698d44d
Thineshkanth <thineshkanth3759@gmail.com>
2022-10-23 10:38:27 +0530

    add a azuredemo frontend part

---
+++ flash-front-end/src/AzureDemo.js
@@ -0,0 +1,18 @@
+// azure sample
+
+import React, {useState} from 'react'
+
+const AzureDemo = () => {
+
+    return(
+        <div>
+            <p>
+            <input onChange={this.onFileChange} type="file"></input>
+                <button onClick={()=> this.uploadFile()}>Upload</button>
+            </p>
+        </div>
+
+    )
+}
+
+export default AzureDemo
