8f872a9d149e3776d6bb2019625125bd7cf9cbc8
nitharsha <nitharshathev@gmail.com>
2022-10-23 10:27:36 +0530

    add attributes for bank details page

--- flash-back-end/src/main/java/com/example/flashbackend/DTO/AddBankDetail.java
+++ flash-back-end/src/main/java/com/example/flashbackend/DTO/AddBankDetail.java
@@ -1,4 +1,31 @@
 package com.example.flashbackend.DTO;
 
+import java.sql.Date;
+
 public class AddBankDetail {
+
+    private Integer cardNo;
+
+    private Integer cvcNo;
+
+    private Date expiaryDate;
+
+    private String cardType;
+
+    public Integer getCardNo() {
+        return cardNo;
+    }
+
+    public Integer getCvcNo() {
+        return cvcNo;
+    }
+
+    public Date getExpiaryDate() {
+        return expiaryDate;
+    }
+
+    public String getCardType() {
+        return cardType;
+    }
+
 }
