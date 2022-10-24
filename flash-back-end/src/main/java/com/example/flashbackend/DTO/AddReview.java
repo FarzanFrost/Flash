9d048225c620ca9f14754e27b2952f0f88f43122
nitharsha <nitharshathev@gmail.com>
2022-10-24 21:18:00 +0530

    remove unwanted codes

--- flash-back-end/src/main/java/com/example/flashbackend/DTO/AddReview.java
+++ flash-back-end/src/main/java/com/example/flashbackend/DTO/AddReview.java
@@ -1,30 +1,21 @@
 package com.example.flashbackend.DTO;
 
-import java.sql.Timestamp;
-
 public class AddReview {
 
     private String comment;
-
-    private Timestamp dateTime;
 
     private Integer rate;
 
     private Boolean deleted;
 
-    public AddReview(String comment, Timestamp dateTime, Integer rate, Boolean deleted){
+    public AddReview(String comment, Integer rate, Boolean deleted){
         this.comment = comment;
-        this.dateTime = dateTime;
         this.rate = rate;
         this.deleted = deleted;
     }
 
     public String getComment() {
         return comment;
-    }
-
-    public Timestamp getDateTime() {
-        return dateTime;
     }
 
     public Integer getRate() {
