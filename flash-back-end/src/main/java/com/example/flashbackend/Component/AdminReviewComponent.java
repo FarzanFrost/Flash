096abb387cd3a4b72b235e6552b44e6d29efa6e6
Afrah <afrahnazeem@gmail.com>
2022-10-26 11:54:05 +0530

    admin review

--- flash-back-end/src/main/java/com/example/flashbackend/Component/AdminReviewComponent.java
+++ flash-back-end/src/main/java/com/example/flashbackend/Component/AdminReviewComponent.java
@@ -1,12 +1,18 @@
 package com.example.flashbackend.Component;
 
-import com.example.flashbackend.Entity.Reviews;
+import com.example.flashbackend.DAO.*;
+import com.example.flashbackend.Entity.*;
 import org.springframework.stereotype.Component;
 import java.math.BigInteger;
 import java.util.List;
-
-import com.example.flashbackend.DAO.*;
 import org.springframework.beans.factory.annotation.Autowired;
+//import com.example.flashbackend.DAO.*;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.stereotype.Component;
+//
+//import java.math.BigInteger;
+//import java.util.List;
+
 
 @Component
 public class AdminReviewComponent {
