01ecccb7b3701c23f308c3625291edc17bd64b19
Thineshkanth <thineshkanth3759@gmail.com>
2022-10-26 15:27:22 +0530

    galleryimages add

--- flash-back-end/src/main/java/com/example/flashbackend/DAO/GalleryImagesRepository.java
+++ flash-back-end/src/main/java/com/example/flashbackend/DAO/GalleryImagesRepository.java
@@ -1,9 +1,19 @@
 package com.example.flashbackend.DAO;
 
 import com.example.flashbackend.Entity.GalleryImages;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
+import javax.transaction.Transactional;
 import java.math.BigInteger;
 
-public interface GalleryImagesRepository extends CrudRepository< GalleryImages , BigInteger > {
+public interface GalleryImagesRepository extends JpaRepository< GalleryImages , BigInteger > {
+
+    @Modifying
+    @Query( value = "INSERT INTO GalleryImages (EventID, ImageUrl) VALUES (  :eventID, :imageurl );" , nativeQuery = true)
+    @Transactional
+    void insertPhotos(@Param("eventID") BigInteger eventID , @Param("imageurl") String imageurl);
 }
