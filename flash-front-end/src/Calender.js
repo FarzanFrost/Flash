139e09d49e5c7b7f89a9149c7902f5072a837f21
Thineshkanth <thineshkanth3759@gmail.com>
2022-10-27 02:52:04 +0530

    add the sweet popup for calender

--- flash-front-end/src/Calender.js
+++ flash-front-end/src/Calender.js
@@ -6,6 +6,7 @@ import axios from "axios";
 import * as events from "events";
 import Button from "react-bootstrap/Button";
 import isBtcAddress from "validator/es/lib/isBtcAddress";
+import Swal from 'sweetalert2'
 
 
 
@@ -14,10 +15,14 @@ const Calender = () => {
 
     const calenderdetails=[]
 
+    const ideventdetailsarray=[]
+
     const serverLink = 'http://localhost:8080'
     const [show, setShow] = useState(false);
 
     const [ eventDetails , setEventDetails ] = useState( null )
+
+    const [ideventdetails,setideventdetails]=useState(null)
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
@@ -42,6 +47,7 @@ const Calender = () => {
 
     } ,[])
 
+
     { eventDetails!== null &&
     eventDetails.map(
         (events)=>{
@@ -58,11 +64,74 @@ const Calender = () => {
     )
     }
 
+
+
+
+    useEffect( () => {
+
+
+
+    } ,[])
+
+    { ideventdetails!== null &&
+    ideventdetails.map(
+        (idevents)=>{
+
+            const ideventsdetails={
+                "eventID":idevents.eventID,
+                "category":idevents.category,
+                "address":idevents.address,
+                "advanceAmount":idevents.advanceAmount,
+                "extraPage":idevents.extraPage
+
+            }
+
+            ideventdetailsarray.push(ideventsdetails)
+        }
+
+    )
+    }
+
+
+
 console.log(calenderdetails)
 
-    const displayEventDetails = ( id ) => {
+    const displayEventDetails = ( eventID ) => {
 
-        alert( id.split("-")[1]  )
+        // alert( eventID.split("-")[1] )
+
+        axios.get( serverLink + '/getImagesByEventId/' + eventID.split("-")[1] ).then(
+
+            ( response ) => {
+
+                console.log( "calender " , response.data)
+                Swal.fire({
+                    title: '<strong>HTML <u>example</u></strong>',
+                    icon: 'info',
+                    html:
+                        'You can use <b>bold text</b>, ' +
+                        '<a href="//sweetalert2.github.io">links</a> ' +
+                        'and other HTML tags',
+                    showCloseButton: true,
+                    showCancelButton: false,
+                    focusConfirm: false,
+                    // confirmButtonText:
+                    //     '<i class="fa fa-thumbs-up"></i> Great!',
+                    // confirmButtonAriaLabel: 'Thumbs up, great!',
+                    // cancelButtonText:
+                    //     '<i class="fa fa-thumbs-down"></i>',
+                    // cancelButtonAriaLabel: 'Thumbs down'
+                })
+
+            }
+
+        ).catch(
+
+            () => { alert( "Error!!! eventidgetdetailserror ") }
+
+        )
+
+
 
     }
         return (
@@ -76,6 +145,8 @@ console.log(calenderdetails)
         eventClick={
             function(arg){
                 displayEventDetails(arg.event.title)
+                console.log("mudilla "+ ideventdetailsarray)
+
                 // displayEventDetails(arg.event.start)
             }
         }
