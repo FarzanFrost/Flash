7c1d09c4d828d90f2a35b24cc27f77d9fddc8cab
SrimohanRahuram <rahuram66@gmail.com>
2022-10-26 18:14:41 -0800

    changing errers

--- flash-front-end/src/AdminViewEmployeeDetailspage.js
+++ flash-front-end/src/AdminViewEmployeeDetailspage.js
@@ -136,7 +136,7 @@ const AdminViewEmployeeDetailspage = () => {
 
                                                 <tr>
                                                     <th scope="col">{ employeeCount++ }</th>
-                                                    <th scope="col"> { employee.firstName } </th>
+                                                    <th scope="col">{ employee.firstName }</th>
                                                     <th scope="col">{ employee.nic }</th>
                                                     <th scope="col">{ employee.gender }</th>
                                                     <th scope="col">{ employee.contactNo }</th>
@@ -148,7 +148,7 @@ const AdminViewEmployeeDetailspage = () => {
 
                                                         <Modal show={show} onHide={handleClose} size="lg">
                                                             <Modal.Header closeButton>
-                                                                <Modal.Title>Employee</Modal.Title>
+                                                                <Modal.Title>Review</Modal.Title>
                                                             </Modal.Header>
                                                             <Modal.Body>
                                                                 <Modal.Body>
@@ -165,32 +165,23 @@ const AdminViewEmployeeDetailspage = () => {
                                                                                     <th scope="col">NO</th>
                                                                                     <th scope="col">Firstname</th>
                                                                                     <th scope="col">Lastname</th>
-                                                                                    <th scope="col">email</th>
                                                                                     <th scope="col">NIC</th>
                                                                                     <th scope="col">Gender</th>
                                                                                     <th scope="col">Contact No</th>
                                                                                     <th scope="col">Type</th>
-                                                                                    <th scope="col">Joined Date & Time</th>
-
-
-
+                                                                                    <th scope="col">Address</th>
                                                                                 </tr>
                                                                                 </thead>
                                                                                 <tbody>
                                                                                 <tr>
-                                                                                    <th scope="col">{ employeeCount }</th>
+                                                                                    <th scope="col">{ employee.employeeID }</th>
                                                                                     <th scope="col">{ employee.firstName }</th>
                                                                                     <th scope="col">{ employee.lastName }</th>
-                                                                                    <th scope="col">{ employee.login.email }</th>
                                                                                     <th scope="col">{ employee.nic }</th>
                                                                                     <th scope="col">{ employee.gender }</th>
                                                                                     <th scope="col">{ employee.contactNo }</th>
                                                                                     <th scope="col">{ employee.type }</th>
-                                                                                    <th scope="col">{ employee.login.dateTime }</th>
-
-
-
-
+                                                                                    <th scope="col">{ employee.address }</th>
                                                                                 </tr>
                                                                                 </tbody>
                                                                             </table>
@@ -201,8 +192,8 @@ const AdminViewEmployeeDetailspage = () => {
 
                                                             </Modal.Body>
                                                             <Modal.Footer>
-                                                                <Button variant="dark" onClick={ () => deleteEmployee( employee.employeeID ) }>
-                                                                    Delete Employee
+                                                                <Button variant="dark" onClick={() => deleteEmployee( employee.employeeID ) }>
+                                                                    Delete Review
                                                                 </Button>
                                                                 <Button variant="dark" onClick={handleClose}>
                                                                     close
@@ -210,13 +201,9 @@ const AdminViewEmployeeDetailspage = () => {
                                                             </Modal.Footer>
                                                         </Modal>
                                                     </th>
-
                                                 </tr>
-
                                             )
-
                                         )
-
                                     }
                                     </tbody>
                                 </table>
