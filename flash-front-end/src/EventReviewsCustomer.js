c22e0c04c867fb9a7e09b89a966a7479258bcb94
SrimohanRahuram <rahuram66@gmail.com>
2022-10-26 19:48:17 -0800

    view review

--- flash-front-end/src/EventReviewsCustomer.js
+++ flash-front-end/src/EventReviewsCustomer.js
@@ -5,13 +5,28 @@ import {AuthenticationContext} from "./ContextFiles/Authentication/Authenticatio
 import axios from "axios";
 
 const EventReviewsCustomer = () => {
-
+    const serverLink = 'http://localhost:8080'
     const { changeContentVisible } = useContext( AuthenticationContext )
 
-    //const [reviewDetails, setReviewDetails] = useState(null)
+    const [reviewDetails, setReviewDetails] = useState(null)
 
-    const { reviews } = useContext( EventReviewsCustomerContext )
+    //const { reviews } = useContext( EventReviewsCustomerContext )
 
+    useEffect( () => {
+            axios.get( serverLink + '/AllReviews').then(
+                ( response ) => {
+
+                    setReviewDetails( response.data )
+                    console.log( response.data )
+
+                }
+            ).catch(
+                () => {alert("Error!!! All Reviews")}
+            )
+
+        }, []
+
+    )
     const star = "bi bi-star text-warning"
 
     const fillStar = "bi bi-star-fill text-warning"
@@ -32,16 +47,18 @@ const EventReviewsCustomer = () => {
 
                     <div className="row text-center">
 
-                        { reviews.length === 0 && <div> No Event Reviews Available </div> }
-                        { reviews.length > 0 &&
+                        {/*{ reviews.length === 0 && <div> No Event Reviews Available </div> }*/}
+                        { reviewDetails !== null &&
 
-                            reviews.map( ( reviews ) => (
+                            reviewDetails.map(
+
+                                ( reviews ) => (
 
                                 <div className="col-md-4 my-3 shadow-lg p-4">
 
                                     <div className="d-flex">
 
-                                        <h2 className="h2 m-auto"> { reviews.eventType } </h2>
+                                        <h2 className="h2 m-auto"> { reviews.reviewsID } </h2>
 
                                         <div className="dropdown m-auto me-0">
                                             <button className="btn bi-three-dots-vertical rounded-circle" type="button" data-bs-toggle="dropdown"
