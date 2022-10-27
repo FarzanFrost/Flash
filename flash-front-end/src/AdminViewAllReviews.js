093f9328303163e9bd23292f56842db5beb72753
SrimohanRahuram <rahuram66@gmail.com>
2022-10-27 01:13:49 -0800

    deleting errors

--- flash-front-end/src/AdminViewAllReviews.js
+++ flash-front-end/src/AdminViewAllReviews.js
@@ -23,49 +23,7 @@ const AdminViewAllReviews = () => {
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
-    const AdminDeleteReview = ( id ) => {
 
-        axios.post( serverLink + '/AdminDeleteReview' , id-- ).then(
-
-            ( response ) => {
-
-                if ( response.data === "done" ){
-
-                    setShow(false);
-
-                }
-
-            }
-
-        ).catch(
-
-            () => { alert( "Error!!! in deleting review") }
-
-        )
-
-    }
-
-    const deleteAdminReview = ( id ) => {
-
-        axios.post( serverLink + '/deleteAdminReview' , id-- ).then(
-
-            ( response ) => {
-
-                if ( response.data === "done" ){
-
-                    setShow(false);
-
-                }
-
-            }
-
-        ).catch(
-
-            () => { alert( "Error!!! add review") }
-
-        )
-
-    }
 
     useEffect( () => {
 
